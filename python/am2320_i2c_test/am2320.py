1c4add515b4b88ea29a9ec51b151e29e99490c31
ugcgeta <ugcgeta@gmail.com>
2016-12-12 22:00:06 +0900

    changing func

--- python/am2320_i2c_test/am2320.py
+++ python/am2320_i2c_test/am2320.py
@@ -6,28 +6,58 @@ import struct
 from time import sleep
 from datetime import datetime
 
-AM2320_I2C_SLEEP_ERROR = -1
+_AM2320_I2C_SLEEP_ERROR = -1
+_Am2320 = 0
+_I2c = 0
+def Am2320_init(i2c):
+    global _Am2320
+    global _I2c
+
+    _I2c = i2c
+    _Am2320 = _I2c.setup(0x5C)
+
+
+def Am2320_getSensor():
+    
+    # ダミーリードでセンサを起こす
+    while(_I2c.writeReg16(_Am2320,0x03,0x0600)==_AM2320_I2C_SLEEP_ERROR):
+        pass
+    
+    # read data controlDatax2 wetDatax2 TempDatax2
+    dataArray = struct.unpack('6B',os.read(am2320,6))
+        
+    Humidity = (float)((dataArray[2]<<8) | dataArray[3])/10
+    temperature = (float)((dataArray[4]<<8) | dataArray[5])/10
+
+    return (temperature,Humidity)
+
+def Am2320_close():
+    global _Am2320
+
+    os.close(_Am2320)
+
+#############################################
 if __name__ == '__main__':
     
     wiringpi2.wiringPiSetup()
     i2c = wiringpi2.I2C()
-    am2320 = i2c.setup(0x5C)
+#    am2320 = i2c.setup(0x5C)
+
+    Am2320_init(i2c)
 
     #for var in range(0, 100):
     while(True):
-        while(i2c.writeReg16(am2320,0x03,0x0600)==AM2320_I2C_SLEEP_ERROR):
-            pass
-            #print("--")
-        #i2c.writeReg16(am2320,0x03,0x0600)
-        #i2c.writeReg16(am2320,0x03,0x0600)
-    
+#        while(i2c.writeReg16(am2320,0x03,0x0600)==AM2320_I2C_SLEEP_ERROR):
+#            pass
         # read data controlDatax2 wetDatax2 TempDatax2
-        dataArray = struct.unpack('6B',os.read(am2320,6))
+#        dataArray = struct.unpack('6B',os.read(am2320,6))
         
-        wet = (float)((dataArray[2]<<8) | dataArray[3])/10
-        tmp = (float)((dataArray[4]<<8) | dataArray[5])/10
+#        wet = (float)((dataArray[2]<<8) | dataArray[3])/10
+#        tmp = (float)((dataArray[4]<<8) | dataArray[5])/10
+#        print(datetime.now().strftime('%X')," >>>  ",wet,"%  , ",tmp,"'C")
+        tmp,wet = Am2320_getSensor()
         print(datetime.now().strftime('%X')," >>>  ",wet,"%  , ",tmp,"'C")
-        sleep(300)
-
-    os.close(am2320)
+        sleep(10)
+    
+    Am2320_close()
 
