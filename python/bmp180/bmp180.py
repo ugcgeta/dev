f31abab5ab5c2e3732718d1c247f8b465260d25e
ugcgeta <ugcgeta@gmail.com>
2016-12-14 22:16:15 +0900

    add bmp180 device

---
+++ python/bmp180/bmp180.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import Adafruit_BMP.BMP085 as BMP085
+
+_Bmp180 = 0
+def Bmp180_init():
+    global _Bmp180
+
+    _Bmp180 = BMP085.BMP085()
+
+
+def Bmp180_getSensor():
+   
+    temp =_Bmp180.read_temperature()
+    hPa = _Bmp180.read_pressure()/100.0
+    alt = _Bmp180.read_altitude()
+    #print(_Bmp180.read_sealevel_pressure())
+    return (temp,hPa,alt)
+
+#############################################
+if __name__ == '__main__':
+    
+    Bmp180_init()
+    print(Bmp180_getSensor())
+
+
