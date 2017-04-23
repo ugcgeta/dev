3e988f2742075cb16c58ad497eb4e9804671c793
ugcgeta <ugcgeta@gmail.com>
2017-04-23 21:54:04 +0900

    stringX2 ADD

--- python/oled_SSD1306/oled.py
+++ python/oled_SSD1306/oled.py
@@ -90,7 +90,11 @@ def Oled_putString(i2c,y,x,string):
 
         nextFontOffset += fontSize
 
-
+def Oled_putStringX2(i2c,y,x,string):
+    cursolPos = x
+    for font in string:
+        cursolPos = Oled_putCharX2(i2c,y,cursolPos,font)
+        print(cursolPos)
 
 def Oled_putChar(i2c,y,x,string):
     if((y<0) or (y>=4) or (x<0) or (x>=128) ):
@@ -110,6 +114,49 @@ def Oled_putChar(i2c,y,x,string):
         for i in range(startPos , endPos):
             i2c.writeReg8(oled_i2c,0x40,string[i])
 
+def Oled_putCharX2(i2c,y,x,string):
+    if((y<0) or (y>=2) or (x<0) or (x>=96) ):
+        return False
+    
+    oled_i2c = i2c.setup(OLED_I2C_ADDRESS)
+   
+    fontX2 = []
+    for lineX2 in range(2):
+        lineX2start = lineX2 * 8
+        lineX2end = lineX2 * 8 + 8
+        
+        for line in range(2):
+            for i,font in enumerate(string):
+                if((i>=lineX2end) or  (i<lineX2start)):
+                    pass
+                else:
+                    if(line==0):
+                        temp = font & 0x0F
+                    else:
+                        temp = (font & 0xF0)>>4
+                    #print(line,font,temp)
+                    tempX2=0x00
+                    for n in range(4):
+                        if(temp & (0x01<<n)):
+                            tempX2 = tempX2 + (0x03<<(n*2))
+                    fontX2.append(tempX2)
+                    fontX2.append(tempX2)
+
+        
+    fontSize = (int)(len(string)/2)*2
+    for line in range(0,4):
+        i2c.writeReg8(oled_i2c,0x00,0xB0 + y*4 + line )  # 垂直開始位置
+        i2c.writeReg8(oled_i2c,0x00,0x21) # set column addres
+        i2c.writeReg8(oled_i2c,0x00,0x00+x) # start column addres
+        i2c.writeReg8(oled_i2c,0x00,0x7F) # stop column addres 
+        
+        startPos = 0 + fontSize * line
+        endPos   = fontSize + fontSize * line
+        for i in range(startPos , endPos):
+            i2c.writeReg8(oled_i2c,0x40,fontX2[i])
+    #print(fontX2)
+    return x+fontSize
+
 
 ################################################################
 if __name__ == '__main__':
@@ -120,6 +167,8 @@ if __name__ == '__main__':
     
     Oled_init(i2c)
 
+    #Oled_putCharX2(i2c,0,0,[0, 192, 56, 6, 56, 192, 0, 0, 60, 3, 2, 2, 2, 3, 60, 0])
     #Oled_putChar(i2c,1,1,(0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xf))
     
-    Oled_putString(i2c,1,1,[[0, 192, 56, 6, 56, 192, 0, 0, 60, 3, 2, 2, 2, 3, 60, 0], [254, 130, 130, 130, 196, 56, 0, 0, 63, 32, 32, 32, 32, 17, 14, 0], [240, 12, 2, 2, 2, 4, 8, 0, 7, 24, 32, 32, 32, 16, 8, 0]])
+    Oled_putStringX2(i2c,0,1,[[0, 192, 56, 6, 56, 192, 0, 0, 60, 3, 2, 2, 2, 3, 60, 0], [254, 130, 130, 130, 196, 56, 0, 0, 63, 32, 32, 32, 32, 17, 14, 0], [240, 12, 2, 2, 2, 4, 8, 0, 7, 24, 32, 32, 32, 16, 8, 0]])
+    #Oled_putString(i2c,2,1,[[0, 192, 56, 6, 56, 192, 0, 0, 60, 3, 2, 2, 2, 3, 60, 0], [254, 130, 130, 130, 196, 56, 0, 0, 63, 32, 32, 32, 32, 17, 14, 0], [240, 12, 2, 2, 2, 4, 8, 0, 7, 24, 32, 32, 32, 16, 8, 0]])
