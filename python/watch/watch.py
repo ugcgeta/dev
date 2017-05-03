0057503666132a84cc02ba36bfe381aaa0fa5d1a
ugcgeta <ugcgeta@gmail.com>
2017-05-03 22:54:27 +0900

    errortrap

--- python/watch/watch.py
+++ python/watch/watch.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import wiringpi
 from time import sleep
 from datetime import datetime
 from am2320 import *
@@ -15,6 +16,12 @@ def _clip(inVal,minVal,maxVal):
     else:
         return inVal
 
+def makeLogFileName():
+    today = datetime.today()
+    logFileName = today.strftime("%Y%m%d_%H%M%S")+'.log'
+    print("Log FileName = ", logFileName)
+
+    return logFileName
 
 #############################################
 if __name__ == '__main__':
@@ -25,27 +32,34 @@ if __name__ == '__main__':
     Am2320_init(i2c)
     Oled_init(i2c)
     Bmp180_init(BMP085.BMP085_ULTRAHIGHRES)
-
-    today = datetime.today()
-    print('Log FileName = ',today.strftime("%Y%m%d_%H%M%S"),".log")
-    f = open(today.strftime("%Y%m%d_%H%M%S")+'.log', 'w')
+    
+    #today = datetime.today()
+    #logFileName = today.strftime("%Y%m%d_%H%M%S")+'.log'
+    #print("Log FileName = ", logFileName)
+    f = open(makeLogFileName(), 'w')
     
     sleep(1)
     while(True):
+        nowTime = datetime.now().strftime('%H:%M')
+
         tmp,wet = Am2320_getSensor()
-        tmpBmp,hPa,alt = Bmp180_getSensor()
+        try:
+            tmpBmp,hPa,alt = Bmp180_getSensor()
+        except:
+            i2c.write(0x00,0x06)  # reset I2C
 
         tmpString = ("%02.1f" % tmp)
         wetString = ("%02.1f" % wet)
         hPaString = ("%04.2f" % hPa)
-        
-        nowTime = datetime.now().strftime('%H:%M')
-        
-        Oled_putStringX2(i2c,0,20,Font_getString(nowTime))
-        Oled_putString(i2c,2,2,Font_getString(tmpString+"℃"))
-        Oled_putString(i2c,2,70,Font_getString(wetString+"％"))
-        Oled_putString(i2c,3,0,Font_getString(hPaString+"hPa"))
-        
+       
+        try:
+            Oled_putStringX2(i2c,0,20,Font_getString(nowTime))
+            Oled_putString(i2c,2,2,Font_getString(tmpString+"℃"))
+            Oled_putString(i2c,2,70,Font_getString(wetString+"％"))
+            Oled_putString(i2c,3,0,Font_getString(hPaString+"hPa"))
+        except:
+            i2c.write(0x00,0x06)  # reset I2C
+
         logTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         f.write(logTime+' '+tmpString+"℃ "+ wetString+"％ " + hPaString+"hPa" + '\n')
         print(logTime+' '+tmpString+"℃ "+ wetString+"％ " + hPaString+"hPa")
@@ -53,9 +67,9 @@ if __name__ == '__main__':
 
         for secCount in range(20):
             if((secCount%2) == 0):
-                Oled_putString(i2c,3,119,Font_getString("."))
+                Oled_putString(i2c,3,100,Font_getString("----"))
             else:
-                Oled_putString(i2c,3,119,Font_getString(" "))
+                Oled_putString(i2c,3,100,Font_getString("    "))
             sleep(1)
 
     f.close()
