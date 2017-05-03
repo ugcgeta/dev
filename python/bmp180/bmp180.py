4501856855ab0e798af5376254073d19a79dbbcc
ugcgeta <ugcgeta@gmail.com>
2017-05-03 22:34:07 +0900

    errortrap

--- python/bmp180/bmp180.py
+++ python/bmp180/bmp180.py
@@ -11,16 +11,30 @@ def Bmp180_init(mode = BMP085.BMP085_STANDARD):
 
 def Bmp180_getSensor():
    
-    temp =_Bmp180.read_temperature()
-    hPa = _Bmp180.read_pressure()/100.0
-    alt = _Bmp180.read_altitude()
-    #print(_Bmp180.read_sealevel_pressure())
+    try:
+        temp =_Bmp180.read_temperature()
+        hPa = _Bmp180.read_pressure()/100.0
+        alt = _Bmp180.read_altitude()
+        #print(_Bmp180.read_sealevel_pressure())
+    except:
+        print("I2C_ERROR_BMP180")
+        temp = 0
+        hPa = 0
+        alt = 0
+        raise
     return (temp,hPa,alt)
 
 #############################################
 if __name__ == '__main__':
     
     Bmp180_init()
-    print(Bmp180_getSensor())
+    while(1):
+        try:
+            print(Bmp180_getSensor())
+        except:
+            print("err")
+        
+        print("-----")
+
 
 
